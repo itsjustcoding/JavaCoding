e2d06f28556bc3b98f24e69980bdc6f946cb84ae
Shubham Chaubey <shubhamchaubey@Shubhams-MacBook-Air.local>
2022-08-23 12:25:54 +0530

    Changes Done

--- myLove.java
+++ myLove.java
@@ -1,20 +1,18 @@
-
-
 package JavaCoding;
 
 import java.util.*;
 public class myLove {
     public static void main(String []args)
     {
-        Scanner sc= new Scanner(System.in);
+        Scanner s = new Scanner(System.in);
         System.out.println("Enter the name= ");
-        String name= sc.nextLine();
-        if(name=="Mridani")
+        String name= s.next();
+        if(name.equals("Mridani") || name.equals("mridani"))
         {
             System.out.println("Hello My Love "+name);
         }
-        else if
-        System.out.println("Have a nice day");
+        else
+        System.out.println("Have a nice day"+name);
     }
     
 }
