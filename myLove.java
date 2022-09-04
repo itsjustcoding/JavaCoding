4ed57ae08725f850f01703b9809b0a0a4357ba18
Shubham Chaubey <111698836+itsjustcoding@users.noreply.github.com>
2022-09-04 11:54:03 +0530

    Update myLove.java

--- myLove.java
+++ myLove.java
@@ -7,12 +7,12 @@ public class myLove {
         Scanner s = new Scanner(System.in);
         System.out.println("Enter the name= ");
         String name= s.next();
-        if(name.equals("Mridani") || name.equals("mridani"))
+        if(name.equals("JAVA") || name.equals("JAVA"))
         {
             System.out.println("Hello My Love "+name);
         }
         else
-        System.out.println("Have a nice day"+name);
+        System.out.println(+name);
     }
     
 }
